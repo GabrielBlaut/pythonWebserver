f79b2f83c7ece170a040828bc92214ddbf1d49a6
Gabriel Blaut <gabriel@linux.fritz.box>
2020-05-06 10:35:19 +0000

    Checks path with error response. Server runs 4ever

--- webserver.py
+++ webserver.py
@@ -3,15 +3,30 @@ from http.server import HTTPServer,BaseHTTPRequestHandler
 
 class myRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"<html><body>")
-        self.wfile.write(b"<b>Hallo</b>")
-        self.wfile.write(b"</html></body>") 
+        try:
+            if self.path.endswith("/"):
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(b"<html><body>")
+                self.wfile.write(b"<b>Hallo</b>")
+                self.wfile.write(b"</html></body>")
+            else:
+                self.send_error(404,"File not Found %s" % self.path)
+
+        except IOError :
+            self.send_error(404) 
 
 
-myServer = HTTPServer(('',8000), myRequestHandler)
+def main():
+    try:
+        port=8000
+        myServer=HTTPServer(('',port),myRequestHandler)
+        print ("webserver running on %s" % port)
+        myServer.serve_forever()
 
-for client in range(5):
-   myServer.handle_request()
+    except KeyboardInterrupt:
+        server.socket.close()
+
+if __name__ == "__main__":
+    main()
