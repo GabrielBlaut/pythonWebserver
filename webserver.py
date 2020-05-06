9f2324fc4e8978943b88bfbdc6ca15e7af50ecfb
Gabriel Blaut <gabriel@linux.fritz.box>
2020-05-06 13:53:23 +0000

    /random at end of url shows random picture + error handel

--- webserver.py
+++ webserver.py
@@ -1,21 +1,50 @@
 
 from http.server import HTTPServer,BaseHTTPRequestHandler
+import os
+import random
+
+def get_random_image(imageDirPath):
+    print(imageDirPath)
+    image_path_list = ['/' + image for image in os.listdir(imageDirPath)
+                if not os.path.isdir(imageDirPath+image)]
+    print(image_path_list)           
+    return imageDirPath + random.choice(image_path_list)
 
 class myRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            if self.path.endswith("/"):
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
-                self.wfile.write(b"<html><body>")
-                self.wfile.write(b"<b>Hallo</b>")
-                self.wfile.write(b"</html></body>")
-            else:
-                self.send_error(404,"File not Found %s" % self.path)
+            
+            #current/working/directory
+            root= os.getcwd()
+            full_path = root + self.path
 
-        except IOError :
-            self.send_error(404) 
+            if self.path.endswith("/random"):
+                image_path = get_random_image(root + '/html/images')
+                print(image_path)
+                statinfo = os.stat(image_path)
+                img_size = statinfo.st_size
+                print(img_size)
+
+                self.send_response(200)
+                self.send_header("Content-type", "image/jpg")
+                self.send_header("Content-length", img_size)
+                self.end_headers()
+
+
+                img=open(image_path, 'rb')
+                self.wfile.write(img.read())
+                img.close()
+               
+            elif not os.path.exists(full_path):
+                msg = "File {} not found".format(self.path)
+                self.send_error(404,msg)
+            #elif os.path.isfile(full_path)
+
+
+        except IOError as err :
+            self.send_error(404,err)
+
+
 
 
 def main():
